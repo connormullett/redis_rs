35a557ab5a550b7b50c35eabe00be9bb705127c5
connor <connormullett@gmail.com>
2021-03-06 11:14:33 -0500

    documentation update

--- src/enums.rs
+++ src/enums.rs
@@ -1,13 +1,16 @@
 use std::{error::Error, fmt};
 
 #[derive(Debug)]
+/// An error type that describes a client error
 pub enum RedisError {
     ParseError,
     SocketConnectionError,
 }
 
+#[doc(hidden)]
 impl Error for RedisError {}
 
+#[doc(hidden)]
 impl fmt::Display for RedisError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "An error occured")
--- src/response.rs
+++ src/response.rs
@@ -1,7 +1,11 @@
 use crate::enums::ResponseType;
 
 #[derive(Debug)]
+/// Data associated with a redis RESP response
 pub struct Response {
+    /// The RESP type the response data is
     pub response_type: ResponseType,
+    /// A string representation of the response.
+    /// Use response_type to dynamically parse to the correct type
     pub data: String,
 }
